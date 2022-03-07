f11d1c6950dda28ca98966182b0c50fe133c1022
hohoprince <hoot0512@gmail.com>
2022-03-07 21:37:11 +0900

    Spring Project 생성

---
+++ Server/carrot-market/src/main/java/com/hmsh/carrotmarket/CarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package com.hmsh.carrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMarketApplication.class, args);
+    }
+
+}
---
+++ Server/carrot-market/src/test/java/com/hmsh/carrotmarket/CarrotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmsh.carrotmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
