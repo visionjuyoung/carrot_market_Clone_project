f20dbcef1a9eecf6ac093a3c784e7f67088d3bf7
ZZmarkus <1170313@naver.com>
2022-03-18 14:43:46 +0900

    [프로필 정보 저장] FileUtil클래스 생성

--- Server/carrot-market/src/main/java/com/hmsh/carrotmarket/util/FileUtil.java
+++ Server/carrot-market/src/main/java/com/hmsh/carrotmarket/util/FileUtil.java
@@ -2,12 +2,14 @@ package com.hmsh.carrotmarket.util;
 
 import com.hmsh.carrotmarket.dto.FileDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.util.UUID;
 
 @Slf4j
+@Service
 public class FileUtil {
 
     public File makeNewFileName(MultipartFile file){
@@ -17,7 +19,7 @@ public class FileUtil {
                 .contentType(file.getContentType())
                 .build();
 
-        File newFileName = new File(fileDTO.getUuid() + "_" + fileDTO.getFileName());
+        File newFileName = new File("/home/ubuntu/upload/" + fileDTO.getUuid() + "_" + fileDTO.getFileName());
 
         return newFileName;
     }
