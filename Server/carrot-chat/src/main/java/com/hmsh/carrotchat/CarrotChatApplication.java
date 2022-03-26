d9f9317eaebb96ace18b6c9d13e4a22a8c7e296f
hohoprince <hoot0512@gmail.com>
2022-03-27 01:21:08 +0900

    [carrot-chat]first commit

---
+++ Server/carrot-chat/src/main/java/com/hmsh/carrotchat/CarrotChatApplication.java
@@ -0,0 +1,13 @@
+package com.hmsh.carrotchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotChatApplication.class, args);
+    }
+
+}
